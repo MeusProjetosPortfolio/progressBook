8a4790f8136d10cd1ad298fa3a277c4734e1bf40
Vitor <vitorcostaofficial@gmail.com>
2024-10-10 12:07:58 -0300

    feat: Criado a classe UserMapper para as conversões

--- src/main/java/com/book/progress/data/dto/UserMapper.java
+++ src/main/java/com/book/progress/data/dto/UserMapper.java
@@ -1,4 +1,26 @@
 package com.book.progress.data.dto;
 
+import com.book.progress.model.User;
+
 public class UserMapper {
+
+    public static UserDto toDto(User user){
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        dto.setReaderLevel(user.getReaderLevel());
+
+        return dto;
+    }
+
+    public static User toEntity(UserDto dto){
+        User user = new User();
+        user.setId(dto.getId());
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setReaderLevel(dto.getReaderLevel());
+
+        return user;
+    }
 }
