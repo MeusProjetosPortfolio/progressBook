824d868edc9838b2b2dadac4f0d572b92ad7260e
Vitor <vitorcostaofficial@gmail.com>
2024-10-11 15:06:16 -0300

    fix: corrigi alguns mapeamentos das leituras

--- src/main/java/com/book/progress/data/mapper/ReadingMapper.java
+++ src/main/java/com/book/progress/data/mapper/ReadingMapper.java
@@ -1,14 +1,16 @@
-package com.book.progress.data.dto;
+package com.book.progress.data.mapper;
 
+import com.book.progress.data.dto.ReadingDto;
 import com.book.progress.model.Reading;
 
 public class ReadingMapper {
-    public static ReadingMapper toDto(Reading reading){
+    public static ReadingDto toDto(Reading reading){
         ReadingDto dto = new ReadingDto();
         dto.setId(reading.getId());
         dto.setCurrentPage(reading.getCurrentPage());
         dto.setStartDate(reading.getStartDate());
         dto.setEndDate(reading.getEndDate());
+        dto.setRating(reading.getRating());
 
         if (reading.getUser() != null){
             dto.setUserDto(UserMapper.toDto(reading.getUser()));
@@ -18,6 +20,25 @@ public class ReadingMapper {
             dto.setBookDto(BookMapper.toDto(reading.getBook()));
         }
 
-      //falta progress
+        if (reading.getProgress() != null) {
+            dto.setProgressDto(ProgressMapper.toDto(reading.getProgress()));
+        }
+            return dto;
+
+    }
+
+    public static Reading toEntity(ReadingDto dto){
+        Reading reading = new Reading();
+        reading.setId(dto.getId());
+        reading.setCurrentPage(dto.getCurrentPage());
+        reading.setStartDate(dto.getStartDate());
+        reading.setEndDate(dto.getEndDate());
+        reading.setRating(dto.getRating());
+
+        reading.setUser(UserMapper.toEntity(dto.getUserDto()));
+        reading.setBook(BookMapper.toEntity(dto.getBookDto()));
+        reading.setProgress(ProgressMapper.toEntity(dto.getProgressDto()));
+
+        return reading;
     }
 }
