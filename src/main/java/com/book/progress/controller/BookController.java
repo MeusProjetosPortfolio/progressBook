71c9be4c614c8c1c7d61d9c399654b27b94d7d80
Vitor <vitorcostaofficial@gmail.com>
2024-10-13 10:52:51 -0300

    feat: construção dos endpoints de Book

--- src/main/java/com/book/progress/controller/BookController.java
+++ src/main/java/com/book/progress/controller/BookController.java
@@ -6,12 +6,14 @@ import com.book.progress.model.Book;
 import com.book.progress.service.BookService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@RestController
+@RequestMapping("/api/books")
+@CrossOrigin("*")
 public class BookController {
 
     @Autowired
@@ -34,6 +36,27 @@ public class BookController {
                 .orElseThrow(()-> new EntityNotFoundException("Pessoa não encontrada com o id: " + id));
     }
 
+    @PostMapping
+    public BookDto createBook(@RequestBody BookDto dtoBook){
+        Book book = BookMapper.toEntity(dtoBook);
+        Book bookSave = bookService.saveBook(book);
+        return BookMapper.toDto(bookSave);
+    }
 
+    @PutMapping("/{id}")
+    public  BookDto updateBook(@PathVariable Long id, @RequestBody BookDto dtoBook) {
 
+        if (!bookService.findIdBook(id).isPresent()) {
+            throw new EntityNotFoundException("Pessoa não encontrada com o id: " + id);
+        }
+
+        Book book = BookMapper.toEntity(dtoBook);
+        book.setId(id);
+        Book bookUpdate = bookService.updateBook(book);
+        return BookMapper.toDto(bookUpdate);
+    }
+
+    public void bookDelete(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
 }
