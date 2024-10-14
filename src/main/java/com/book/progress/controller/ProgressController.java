411077fca4c0071e0e67ac466a3784a22823daac
Vitor <vitorcostaofficial@gmail.com>
2024-10-14 13:17:27 -0300

    feat: progressController construída

--- src/main/java/com/book/progress/controller/ProgressController.java
+++ src/main/java/com/book/progress/controller/ProgressController.java
@@ -1,4 +1,56 @@
 package com.book.progress.controller;
 
+import com.book.progress.data.dto.ProgressDto;
+import com.book.progress.data.mapper.ProgressMapper;
+import com.book.progress.model.Progress;
+import com.book.progress.service.ProgressService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/progress")
+@CrossOrigin("*")
 public class ProgressController {
+
+    @Autowired
+    private ProgressService progressService;
+
+    @GetMapping
+    public List<ProgressDto> progressDtoList() {
+        List<Progress> progresses = progressService.findAllProgress();
+
+        return progresses.stream()
+                .map(ProgressMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ProgressDto findByProgressId(@PathVariable Long id){
+
+        return progressService.findIdProgress(id)
+                .map(ProgressMapper::toDto)
+                .orElseThrow(()->new EntityNotFoundException("Não encontrado o progresso com o id " + id));
+    }
+
+    @PutMapping("/{id}")
+
+    public ResponseEntity<ProgressDto> updateProgress(@PathVariable Long id, @RequestBody ProgressDto dtoProgress) {
+
+        if (!progressService.findIdProgress(id).isPresent()) {
+            throw new EntityNotFoundException("Progresso não encontrado com o id " + id);
+        }
+
+        Progress progressUpdate = progressService.updateProgressStatus(id,dtoProgress);
+
+        return ResponseEntity.ok(ProgressMapper.toDto(progressUpdate));
+    }
+
+    public void progressDelete(@PathVariable Long id) {
+        progressService.deleteProgress(id);
+    }
 }
