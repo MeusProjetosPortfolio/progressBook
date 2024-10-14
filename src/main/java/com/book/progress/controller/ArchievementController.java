d236628477148dba0a156db8e4d2d9d597579cd6
Vitor <vitorcostaofficial@gmail.com>
2024-10-14 13:21:47 -0300

    feat: início da construção da classe ArchievementController

--- src/main/java/com/book/progress/controller/ArchievementController.java
+++ src/main/java/com/book/progress/controller/ArchievementController.java
@@ -1,4 +1,19 @@
 package com.book.progress.controller;
 
+import com.book.progress.repository.ArchievementRepository;
+import com.book.progress.service.ArchievementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/archievements")
+@CrossOrigin("*")
 public class ArchievementController {
+
+    @Autowired
+    public ArchievementService archievementService;
+
+
 }
