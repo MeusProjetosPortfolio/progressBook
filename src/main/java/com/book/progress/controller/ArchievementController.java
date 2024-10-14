1285f0d013e4c8c650f6d89e26e2c7e68f90811e
Vitor <vitorcostaofficial@gmail.com>
2024-10-14 17:25:53 -0300

    feat: implementada ArchievementController

--- src/main/java/com/book/progress/controller/ArchievementController.java
+++ src/main/java/com/book/progress/controller/ArchievementController.java
@@ -1,19 +1,64 @@
 package com.book.progress.controller;
 
-import com.book.progress.repository.ArchievementRepository;
+import com.book.progress.data.dto.ArchivementDto;
+import com.book.progress.data.mapper.ArchievementMapper;
+import com.book.progress.model.Archievement;
 import com.book.progress.service.ArchievementService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/archievements")
+@RequestMapping("/archievements")
 @CrossOrigin("*")
 public class ArchievementController {
 
     @Autowired
-    public ArchievementService archievementService;
+    private ArchievementService archievementService;
 
+    @GetMapping
+    public List<ArchivementDto> archivementDtoList() {
+        List<Archievement> archievements = archievementService.findAllArchivement();
 
+        return archievements.stream()
+                .map(ArchievementMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArchivementDto> findArchievementById(@PathVariable Long id) {
+        Optional<Archievement> archievement = archievementService.findIdArchivement(id);
+        return archievement.map(value -> ResponseEntity.ok(ArchievementMapper.toDto(value)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ArchivementDto createArchievement(@RequestBody ArchivementDto dtoArchievement){
+        Archievement archievement = ArchievementMapper.toEntity(dtoArchievement);
+        Archievement archievementSave = archievementService.saveArquievement(archievement);
+        return ArchievementMapper.toDto(archievementSave);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ArchivementDto> updateArchievement(@PathVariable Long id, @RequestBody ArchivementDto dtoArchievement){
+        Archievement newArchievementData = ArchievementMapper.toEntity(dtoArchievement);
+        Optional<Archievement> archievementUpdate = archievementService.updateArchievement(id,newArchievementData);
+
+        return archievementUpdate
+                .map(value -> ResponseEntity.ok(ArchievementMapper.toDto(value)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArchievement(@PathVariable Long id){
+        boolean deleted = archievementService.deleteArchievement(id);
+        if (deleted){
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
