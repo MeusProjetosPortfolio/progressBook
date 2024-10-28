8a848f6db8f29c0a9db857681d5f96e6a7f22479
Vitor <vitorcostaofficial@gmail.com>
2024-10-28 14:08:28 -0300

    feat: adicionada as descrições do meu swagger

--- src/main/java/com/book/progress/ProgressApplication.java
+++ src/main/java/com/book/progress/ProgressApplication.java
@@ -1,7 +1,22 @@
 package com.book.progress;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Backend de incentivo à leitura",
+				version = "1.0",
+				description = "Através de um sistema de recompensas, essa API tem o intuito de incentivar a leitura",
+				contact = @Contact(
+						name = "João Vitor",
+						email = "teste@gmail.com",
+						url = "https://www.google.com")
+		)
+)
 
 @SpringBootApplication
 public class ProgressApplication {
